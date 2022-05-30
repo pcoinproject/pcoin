4d2bc0b98a7baf223a45bfc408891dc33556d3fc
behfarkhosravi <behfar.khosravi@gmail.com>
2022-05-30 20:18:03 +0430

    initial commit

---
+++ src/qt/pivx/masternodeswidget.h
@@ -0,0 +1,70 @@
+// Copyright (c) 2019-2020 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef MASTERNODESWIDGET_H
+#define MASTERNODESWIDGET_H
+
+#include "qt/pivx/pwidget.h"
+#include "qt/pivx/furabstractlistitemdelegate.h"
+#include "qt/pivx/tooltipmenu.h"
+
+#include <atomic>
+
+#include <QTimer>
+#include <QWidget>
+
+class PIVXGUI;
+class MNModel;
+
+namespace Ui {
+class MasterNodesWidget;
+}
+
+QT_BEGIN_NAMESPACE
+class QModelIndex;
+QT_END_NAMESPACE
+
+class MasterNodesWidget : public PWidget
+{
+    Q_OBJECT
+
+public:
+
+    explicit MasterNodesWidget(PIVXGUI *parent = nullptr);
+    ~MasterNodesWidget();
+    void setMNModel(MNModel* _mnModel);
+
+    void run(int type) override;
+    void onError(QString error, int type) override;
+
+    void showEvent(QShowEvent *event) override;
+    void hideEvent(QHideEvent *event) override;
+
+private Q_SLOTS:
+    void onCreateMNClicked();
+    void onStartAllClicked(int type);
+    void changeTheme(bool isLightTheme, QString &theme) override;
+    void onMNClicked(const QModelIndex &index);
+    void onEditMNClicked();
+    void onDeleteMNClicked();
+    void onInfoMNClicked();
+    void updateListState();
+    void updateModelAndInform(const QString& informText);
+
+private:
+    Ui::MasterNodesWidget *ui;
+    FurAbstractListItemDelegate *delegate;
+    MNModel *mnModel = nullptr;
+    TooltipMenu* menu = nullptr;
+    QModelIndex index;
+    QTimer *timer = nullptr;
+
+    std::atomic<bool> isLoading;
+
+    bool checkMNsNetwork();
+    void startAlias(const QString& strAlias);
+    bool startAll(QString& failedMN, bool onlyMissing);
+};
+
+#endif // MASTERNODESWIDGET_H
