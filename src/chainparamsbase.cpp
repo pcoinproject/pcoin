082fe28405f5d746fe82f8d718e22e9225872229
behfarkhosravi <behfar.khosravi@gmail.com>
2022-06-01 03:13:30 +0430

    [Core] Change test rpc port

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -38,7 +38,7 @@ std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain
     if (chain == CBaseChainParams::MAIN)
         return std::make_unique<CBaseChainParams>("", 22502);
     else if (chain == CBaseChainParams::TESTNET)
-        return std::make_unique<CBaseChainParams>("testnet5", 51475);
+        return std::make_unique<CBaseChainParams>("testnet5", 22504);
     else if (chain == CBaseChainParams::REGTEST)
         return std::make_unique<CBaseChainParams>("regtest", 51477);
     else
