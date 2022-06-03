4630662cd4c0957e5bd4507aaf27c4f0c963df32
behfarkhosravi <behfar.khosravi@gmail.com>
2022-06-04 01:13:18 +0430

    Update clientversion.cpp

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -42,11 +42,10 @@ const std::string CLIENT_NAME(PACKAGE_NAME);
 #include "obj/build.h"
 #endif
 
-//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
-#define GIT_ARCHIVE 1
+//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "5a7b5141a7fcf765729ce31b6a7c0ce9b6d21969"
-#define GIT_COMMIT_DATE "Tue, 8 Mar 2022 09:01:13 -0300"
+#define GIT_COMMIT_ID "$Format:%H$"
+#define GIT_COMMIT_DATE "$Format:%cD$"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
